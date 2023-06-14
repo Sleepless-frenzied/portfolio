ee6a47b256ba7fb4af9c0429a5b14a82dca2dd33
Xyphes <willy.somkhit@epita.fr>
2023-06-15 01:43:52 +0200

    some updates

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -26,7 +26,6 @@ export default function Who() {
                        rel="noreferrer noopener">{t('cv')}
                     </a>
                 </div>
-                <br/><br/>
                 <br/>
                 <a href={process.env.PUBLIC_URL + '/Lettre-de-recommandation-Willy-Somkhit.pdf'}
                    className="md:text-xl 2xl:text-5xl rounded-full py-3 px-4  2xl:py-6 2xl:px-8 text-xl font-bold text-center bg-blue-700 text-white "
